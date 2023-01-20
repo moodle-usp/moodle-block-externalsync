c853c1291f414d4f61acca507200f499e0c9343c
Potalej <oapotalej@gmail.com>
2023-01-20 15:59:13 -0300

    Check order + user fields.
    User fields updated to be 'email', 'username', 'firstname' and 'lastname' for now.
    
    The 'checkArray' function now don't use a foreach loop, but some set intuition to check if the fields uploaded are consistant with the expected.

--- utils/csv.php
+++ utils/csv.php
@@ -20,18 +20,16 @@ function checkArray ($array, $type) {
   if ($type == 0)
     $header = ['id', 'code', 'shortname', 'fullname', 'start', 'end', 'summary'];  
   else if ($type == 1)
-    $header = ['cpf', 'email', 'name', 'birthdate', 'mothername'];
+    $header = ['email', 'username', 'firstname', 'lastname'];
+    
   else {
     return false; // TODO: error treatment
   }
   
-  $ii = 0;
-  foreach ($keys as $key) {
-    $header_key = $header[$ii];
-    $ii++;
-    if ($key != $header_key) {
-      return false;
-    }
+  // array_dif
+  if (sizeof($keys) == sizeof($header) and array_diff($keys, $header)==array()) {
+    return true;  
   }
-  return true;
+  
+  return false;
 }
